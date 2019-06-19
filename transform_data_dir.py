c0fd0d2f997a1a9c1c1c12ec3ba2dd11462d7b59
Игорь Черепанов <30562858+ZigorewsLike@users.noreply.github.com>
2019-06-19 14:43:04 +0800

    Update transform_data_dir.py

--- transform_data_dir.py
+++ transform_data_dir.py
@@ -1,13 +1,21 @@
-#############################################################################################
-# Параметры программы:                                                                      #
-#   1 - Путь к папке, в которой находятся директории Annotations и Images                   #
-#   2 - Путь к папке, куда скопировать файлы                                                #
-#   3 - Название папки с xml файлами                                                        #
-#   4 - Название папки с картинками                                                         #
-#   5 - (не обязательно) Значение параметра отображения вывода 'vis', который описан ниже   #
-#                                                                                           #
-# Пример запуска : transform_data_dir.py 'D:\Input Dir' 'D:\Output Dir' Annotation Images 1 #
-#############################################################################################
+""""Program parameters:
+
+1 - The path to the folder where the "Annotations" and "Images" directories are located.
+2 - Path to the folder where to copy files.
+3 - Folder name with xml files.
+4 - The name of the folder with pictures.
+5 - (optional) The value of the display parameter of the 'display_mode' output, which is described below.
+
+Output display option.
+    0 - Display only the completion message.
+    1 - Display information about which files are being copied.
+    2 - Display detailed information for each file.
+
+
+Startup example:
+    transform_data_dir.py 'D:\Input Dir' 'D:\Output Dir' Annotation Images 1
+
+"""
 
 import sys
 import os
@@ -18,48 +26,44 @@ output_folder = 'D:/WORK/OUT'
 folder_ann = 'Annotations'
 folder_img = 'Images'
 
-cout_files = 0
-
-# Значения параметра отображения вывода.
-#   0 - Вывести только сообщение о завершении,
-#   1 - Вывести информацию о том, какие файлы копируются,
-#   2 - Вывести подробную информацию по каждому файлу
+count_files = 0
 
 if len(sys.argv) == 5:
     input_folder = sys.argv[1]
     output_folder = sys.argv[2]
     folder_ann = sys.argv[3]
     folder_img = sys.argv[4]
-    vis = 1
+    display_mode = 1
 else:
     if len(sys.argv) == 6:
         input_folder = sys.argv[1]
         output_folder = sys.argv[2]
         folder_ann = sys.argv[3]
         folder_img = sys.argv[4]
-        vis = int(sys.argv[5])
+        display_mode = int(sys.argv[5])
     else:
         if len(sys.argv) < 5:
-            print("ERROR! Параметров слишком мало")
+            print("ERROR! Not enough parameters")
             sys.exit(1)
         else:
-            print("ERROR! Параметров слишком много")
+            print("ERROR! Too many parameters")
             sys.exit(1)
 
-if vis == 2:
-    print("\nParams:\n-" + sys.argv[1] + "\n-" + sys.argv[2] + "\n-" + sys.argv[3] + "\nLength=" + str(len(sys.argv)) + "\n")
+if display_mode == 2:
+    print("\nParams:\n-" + sys.argv[1] + "\n-" + sys.argv[2] + "\n-" + sys.argv[3] + "\nLength=" + str(
+        len(sys.argv)) + "\n")
 
 try:
     os.mkdir(output_folder + '/annotations')
     os.mkdir(output_folder + '/annotations/xmls')
     os.mkdir(output_folder + '/images')
 except OSError:
-    print("Создать директорию %s не удалось. Возможно она уже существует." % output_folder)
+    print("Create directory %s failed. Maybe it already exists." % output_folder)
 else:
-    if vis != 0:
-        print("Успешно создана директория %s " % output_folder)
+    if display_mode != 0:
+        print("Directory successfully created %s" % output_folder)
 
-Files = os.listdir(input_folder + '/' + folder_ann)
+files = os.listdir(input_folder + '/' + folder_ann)
 
 xml_folder = os.path.join(output_folder, "annotations/xmls")
 trainval_path = os.path.join(output_folder, "annotations")
@@ -67,33 +71,33 @@ img_folder = os.path.join(output_folder, "images")
 
 f = open(trainval_path + '/' + 'trainval.txt', 'w')
 
-for xml_file in Files:
+for xml_file in files:
     if xml_file.endswith('.xml'):
-        cout_files += 1
+        count_files += 1
         ann_path_in = os.path.join(input_folder, folder_ann + '/' + xml_file)
-        if vis == 2:
-            print("Файл xml на входе - " + ann_path_in)
+        if display_mode == 2:
+            print("Xml file - " + ann_path_in)
         ann_path_out = os.path.join(xml_folder, xml_file)
-        if vis == 2:
-            print("Файл xml скопирован - " + ann_path_out)
+        if display_mode == 2:
+            print("Xml file copied - " + ann_path_out)
         file_name = xml_file[:-4]
-        if vis == 2:
-            print("Название файла - " + str(file_name))
+        if display_mode == 2:
+            print("File name - " + str(file_name))
         f.write(str(file_name) + '\n')
 
         img_path_in = os.path.join(input_folder, folder_img + '/' + file_name + '.jpeg')
-        if vis == 2:
-            print("Файл jpeg на входе - " + img_path_in)
+        if display_mode == 2:
+            print("Jpeg file - " + img_path_in)
         img_path_out = os.path.join(img_folder, file_name + '.jpeg')
-        if vis == 2:
-            print("Файл jpeg скопирован - " + img_path_out)
+        if display_mode == 2:
+            print("Jpeg file copied - " + img_path_out)
         shutil.copyfile(ann_path_in, ann_path_out)
         shutil.copyfile(img_path_in, img_path_out)
-        if vis == 2 or vis == 1:
-            print("файл %s (xml и jpeg) скопирован!" % file_name)
+        if display_mode == 2 or display_mode == 1:
+            print("%s file (xml and jpeg) copied!" % file_name)
 
-        if vis == 2:
+        if display_mode == 2:
             print("==================")
 
-print("Копирование завершено!\n Количество файлов : " + str(cout_files))
+print("Copying completed! \n Number of files:" + str(count_files))
 f.close()
